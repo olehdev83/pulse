b4f20173034b6a8c52abece09bb6003a2bcc4d8b
Oleh Dev <oleh.dev83@gmail.com>
2022-11-05 21:04:50 +0100

    Реализовали переключение компонентов табов

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-$('.carousel__inner').slick({
+$(document).ready(function(){
+	$('.carousel__inner').slick({
 		speed: 1200,
 		adaptiveHeight: true,
 		infinite: false,
@@ -21,6 +22,20 @@ $('.carousel__inner').slick({
 				  autoplay: true,
 				  autoplaySpeed: 1200,
 				}
-			  },
+			},
 		]
+	});
+
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+
+	  });
+
 });
+
+
+
+
