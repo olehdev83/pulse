27d0eec3ddbab39145d4871a884a7a33cf67cf8e
Oleh Dev <oleh.dev83@gmail.com>
2022-11-08 18:05:25 +0100

    За адаптировали section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -34,6 +34,19 @@ $(document).ready(function(){
 
 	  });
 
+	  
+	  function toggleTabSlide(nameClass){
+		$(nameClass).each(function(i){
+			$(this).on('click', function(e){
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			});
+		  });
+	  };
+
+	  toggleTabSlide('.catalog-item__link');
+	  toggleTabSlide('.catalog-item__back');
 });
 
 
